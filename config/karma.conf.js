02d670fd20b74141e7cc4a271f4d1b2634d9d99b
Karl Gustav Røksund <kkgg84+git@gmail.com>
2014-02-22 16:44:50 +0100

    Initial commit of a bare minimum angular/karma project (based on the angular seed project, just more cut down)

---
+++ config/karma.conf.js
@@ -0,0 +1,25 @@
+module.exports = function(config){
+    config.set({
+    basePath : '../',
+
+    files : [
+      'app/lib/angular/angular.js',
+      'app/lib/angular/angular-*.js',
+      'test/lib/angular/angular-mocks.js',
+      'app/js/**/*.js',
+      'test/unit/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-junit-reporter',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine'
+            ]
+})}
